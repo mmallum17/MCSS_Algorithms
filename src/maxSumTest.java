8a5bdaea52a14bb05fd7fcb2e55a4b19ee0daaeb
Marcus <marcusmallum@gmail.com>
2017-02-10 22:52:38 -0600

    Generate Random Numbers

--- src/maxSumTest.java
+++ src/maxSumTest.java
@@ -1,6 +1,30 @@
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * Created by Marcus on 2/10/2017.
  */
 public class maxSumTest
 {
+    public static void main(String[] args)
+    {
+        int size = 0;
+        int randomNumbers[];
+        Random random = new Random();
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter the size of the problem (N): ");
+        size = input.nextInt();
+        randomNumbers = new int[size];
+
+        // Generate random numbers
+        for(int i = 0; i < size; i++)
+        {
+            randomNumbers[i] = random.nextInt(19999) - 9999;
+            if(size < 50)
+            {
+                System.out.printf("%d\n", randomNumbers[i]);
+            }
+        }
+    }
 }
